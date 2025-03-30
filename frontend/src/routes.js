a35125c3c5d4465ae470dd057d04a9ee611418d0
Elena <utv.header@gmail.com>
2025-03-30 14:47:50 +0300

    Change frontend part for deploy on Render

--- frontend/src/routes.js
+++ frontend/src/routes.js
@@ -1,13 +1,13 @@
 import axios from "axios";
 
 // const apiPath = 'api';
-const authPath = 'auth';
+// const authPath = 'auth';
 const baseURL = process.env.REACT_APP_API_URL || 'http://127.0.0.1:5000';
 
 const routes = {
   main: () => `${baseURL}/`,
-  login: () => `${baseURL}/${authPath}/login`,
-  signup: () => `${baseURL}/${authPath}/register`,
+  login: () => `${baseURL}/login`,
+  signup: () => `${baseURL}/register`,
 };
 
 export default routes;
