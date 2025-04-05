96b200bae930aea4d44d4a9ecc7db6c3af10ca86
Elena <utv.header@gmail.com>
2025-04-05 18:34:50 +0300

    Change for deploy

--- backend/db.js
+++ backend/db.js
@@ -11,11 +11,16 @@ const pool = new Pool({
 
 // Простой запрос к базе данных для проверки
 pool.query('SELECT NOW()', (err, result) => {
+    console.log('Подключены к БД:', err ? err : res.rows[0].current_database);
     if (err) {
       console.error('Ошибка подключения к БД:', err);
     } else {
       console.log('Успешное подключение к PostgreSQL. Текущее время:', result.rows[0].now);
     }
+});
+
+pool.query('SELECT current_database()', (err, res) => {
+  console.log('Подключены к БД:', err ? err : res.rows[0].current_database);
 });
 
 // Экспорт pool для использования в других файлах
