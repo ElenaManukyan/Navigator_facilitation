f4071885bbc0604543d61a36860a453049582830
Elena <utv.header@gmail.com>
2025-04-06 15:47:02 +0300

    Change server

--- backend/server.js
+++ backend/server.js
@@ -92,6 +92,6 @@ app.use((req, res) => {
 
 const PORT = process.env.PORT || 5000;
 // Запуск сервера
-app.listen(PORT, '0.0.0.0', () => {
+app.listen(PORT, () => {
     console.log(`Сервер запущен на порту: ${PORT}`);
 });
